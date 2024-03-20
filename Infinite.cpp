f5f57d9329189aff512a191674caf5cf8880ceff
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2024-03-20 14:49:11 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     while(true)     {         int n;         cin>>n;         if(n==-1)         {             break;         }         else         {             cout<<n*n<<endl;         }     } }

---
+++ Infinite.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==-1)
+        {
+            break;
+        }
+        else
+        {
+            cout<<n*n<<endl;
+        }
+    }
+}
